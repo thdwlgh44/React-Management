742bbac43feda678ade4e53c5bfd5af4174410d1
thdwlgh44 <thdwlgh44@naver.com>
2023-05-11 10:34:28 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '소지호',
+  'birthday': '960923',
+  'gender': '남자',
+  'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '960923',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '이순신',
+      'birthday': '960923',
+      'gender': '남자',
+      'job': '디자이너'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
+   <div>
+     {
+       customers.map(c => {
+        return <Customer
+         key={c.id} 
+         id={c.id}
+         image={c.image}
+         name={c.name}
+         birthday={c.birthday}
+         gender={c.gender}
+         job={c.job}
+        />
+       })
+     }
     </div>
   );
 }
